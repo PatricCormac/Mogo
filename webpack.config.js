bdfa1f4f884a6170de9537e50eaa91a2ad69382f
PatricCormac <socolov1897@gmail.com>
2020-08-11 00:52:04 +0500

    Completed header

---
+++ webpack.config.js
@@ -0,0 +1,169 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserWebpackPlugin = require("terser-webpack-plugin");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+const ImageminPlugin = require("imagemin-webpack-plugin").default;
+
+const isDev = process.env.NODE_ENV === "development";
+
+const optimization = () => {
+  const config = {
+    splitChunks: {
+      chunks: "all",
+    },
+  };
+
+  if (!isDev) {
+    config.minimizer = [
+      new OptimizeCssAssetsPlugin(),
+      new TerserWebpackPlugin(),
+    ];
+  }
+
+  return config;
+};
+
+const filename = (path, ext) =>
+  isDev ? `${path}/[name].${ext}` : `${path}/[name].[hash].${ext}`;
+
+const cssLoaders = (extra) => {
+  const loaders = [
+    {
+      loader: MiniCssExtractPlugin.loader,
+      options: {
+        hmr: isDev,
+        reloadAll: true,
+      },
+    },
+    "css-loader",
+    {
+      loader: "postcss-loader",
+      options: { sourceMap: true, config: { path: "postcss.config.js" } },
+    },
+  ];
+
+  if (extra) {
+    loaders.push(extra);
+  }
+
+  return loaders;
+};
+
+const babelOtions = (preset) => {
+  const opts = {
+    presets: ["@babel/preset-env"],
+  };
+
+  if (preset) {
+    opts.presets.push(preset);
+  }
+
+  return opts;
+};
+
+const jsLoaders = () => {
+  const loaders = [
+    {
+      loader: "babel-loader",
+      options: babelOtions(),
+    },
+  ];
+
+  if (isDev) {
+    loaders.push("eslint-loader");
+  }
+
+  return loaders;
+};
+
+const plugins = () => {
+  const base = [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+      minify: {
+        collapseWhitespace: !isDev,
+      },
+    }),
+    new CleanWebpackPlugin(),
+    /*new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src/favicon.ico"),
+          to: path.resolve(__dirname, "dist"),
+        },
+      ],
+    }),*/
+    new ImageminPlugin({ test: /\.(jpe?g|png|gif|svg)$/i }),
+    new MiniCssExtractPlugin({
+      filename: filename("styles", "css"),
+    }),
+  ];
+
+  if (!isDev) {
+    base.push(new BundleAnalyzerPlugin());
+  }
+
+  return base;
+};
+
+module.exports = {
+  context: path.resolve(__dirname, "src"),
+  entry: {
+    main: ["@babel/polyfill", "./index.js"],
+    //analytics: "./analytics.js",
+  },
+  output: {
+    filename: filename("js", "js"),
+    path: path.resolve(__dirname, "build"),
+  },
+  optimization: optimization(),
+  devServer: {
+    port: 9000,
+    hot: false,
+  },
+  devtool: isDev ? "source-map" : "",
+  plugins: plugins(),
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: cssLoaders(),
+      },
+      {
+        test: /\.less$/,
+        use: cssLoaders("less-loader"),
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: cssLoaders("sass-loader"),
+      },
+      {
+        test: /\.(ttf|woff|woff2|eot)$/,
+        loader: "file-loader",
+        options: {
+          name: "[name].[ext]",
+          publicPath: "../fonts",
+          outputPath: "fonts",
+        },
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        loader: "file-loader",
+        options: {
+          name: "[name].[ext]",
+          publicPath: "../img",
+          outputPath: "img",
+        },
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: jsLoaders(),
+      },
+    ],
+  },
+};
