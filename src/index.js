8557cada1c1ae5c71b4d260f1c7b1f9a83d567ac
PatricCormac <socolov1897@gmail.com>
2020-09-01 04:46:19 +0500

    Адаптировал несколько секций

--- src/index.js
+++ src/index.js
@@ -1,3 +1,2 @@
-//import * as $ from "jquery";
-import "@fortawesome/fontawesome-free/css/all.css";
-import "./styles/main.scss";
+import '@fortawesome/fontawesome-free/css/all.css';
+import './styles/main.scss';
